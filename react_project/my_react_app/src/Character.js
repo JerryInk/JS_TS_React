cc51f618a08ac3cbd5f2308fd8116857c45fbc9a
JerryInk <kardahin_lesha@list.ru>
2023-06-29 13:41:04 +0500

    Add useState and useEffect

--- react_project/my_react_app/src/Character.js
+++ react_project/my_react_app/src/Character.js
@@ -4,15 +4,15 @@ import './Character.css';
 
 const Character = ({name, status, species}) => (
     <div className={'charcter_item'}>
-        <div className={'character_item_name'}>{name}</div>
-        <div className={'character_item_status'}>
-            { status === 'Alive' && <img src="/heartbeat.png" alt="status .png"></img> }
-            { status === 'Dead' && <img src="/danger.png" alt="status .png"></img> }
-            { status === 'unknown' && <img src="/person.png" alt="status .png"></img> }
+        <div className={'character_item_name character_item_width'}>{name}</div>
+        <div className={'character_item_status character_item_width'}>
+            { status === 'Alive' && <img src="/heartbeat.png" alt="Alive"></img> }
+            { status === 'Dead' && <img src="/danger.png" alt="Dead"></img> }
+            { status === 'unknown' && <img src="/person.png" alt="unknown"></img> }
         </div>
-        <div className={'character_item_species'}>
-            { species === 'Alien' && <img src="/alien.png" alt="species .png"></img> }
-            { species === 'Human' && <img src="/human.png" alt="species .png"></img> }
+        <div className={'character_item_species character_item_width'}>
+            { species === 'Alien' && <img src="/alien.png" alt="Alien"></img> }
+            { species === 'Human' && <img src="/human.png" alt="Human"></img> }
         </div>
     </div>
 );
--- react_project/my_react_app/src/InputComponent.js
+++ react_project/my_react_app/src/InputComponent.js
@@ -2,20 +2,20 @@ import React from "react";
 import './InputComponent.css';
 
 // Компонент для выбора города для отображения погоды
-const InputComponent = () => (
+const InputComponent = ({query, onChange}) => {
+  return (
     <div className={'character_search'}>
       <form className={'character_search_form'}>
         <input 
           className={'character_search_form_input'} 
           type="search" 
-          placeholder="..."
-          onChange={(event) => {
-            console.log(event.target.value)
-          }}>
-
+          placeholder="Фильтрация персонажей..."
+          value={query}
+          onChange={onChange}>
         </input>
       </form>
     </div>
-);
+  );
+};
 
 export default InputComponent;
